207d06115778fb40c75b5b7b4f41b179b1a164b6
JJ_Jacob <yjj.jacob@gmail.com>
2014-11-14 09:41:33 +0800

    add custom filter
    custom filter example

---
+++ src/main/java/com/infosky/servlet/TestServlet.java
@@ -0,0 +1,33 @@
+package com.infosky.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet implements Servlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		performTask(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		performTask(request, response);
+	}
+
+	private void performTask(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("TestServlet says hi");
+	}
+
+}
